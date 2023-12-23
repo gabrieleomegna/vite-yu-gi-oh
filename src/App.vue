c2a80dab35228f2d5b4de87827b51717e6eb014d
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-12-23 23:30:58 +0100

    Implemented macrosctructure of app.vue and created single elements. Started call to api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <script >
+    import {store} from './js/store.js';
+    import axios from 'axios';
+    import AppHeader from './components/AppHeader.vue';
+    import AppSearch from './components/AppSearch.vue';
+    import CardsList from './components/CardsList.vue';
+
+    export default {
+      components : {
+        AppHeader,
+        AppSearch,
+        CardsList,
+      },
+      data () {
+        return {
+          cards : [],
+          store
+        }
+      },
+      methods : {
+        getCards() {
+          axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+          .then((response) => {
+            // handle success
+            console.log(response.data.data);
+          })
+          .catch(function (error) {
+            // handle error
+            console.error(error);
+          })
+        }
+      },
+      created () {
+        this.getCards();
+      },
+    }
 </script>
 
 <template>
-  
+    <AppHeader/>
+    <AppSearch/>
+    <CardsList/>
 </template>
 
 <style lang="scss">
-    
+    @use './styles/general.scss';
 </style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from '../js/store';
+import SingleCard from './SingleCard.vue';
+export default {
+    components: {
+        SingleCard
+    }
+}
+</script>
+
+<template>
+    <SingleCard/>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
